e5777b7ef4379d685a1381390230959981710e4e
Tiago Serra <35219940+N2late@users.noreply.github.com>
2022-07-22 09:13:14 +0200

    add title and logo

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import logo from '../img/logo2.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" src={logo} alt="Reddit Books" />
+        <h1 className="title">Reddit Books</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import App from './app/App';
 import { store } from './app/store';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
