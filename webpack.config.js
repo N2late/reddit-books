044555053d9876fd04e74869d34e36fdc62c23e1
Tiago Serra <35219940+N2late@users.noreply.github.com>
2022-08-27 18:45:13 +0200

    add fallback for buffer

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  resolve: {
+    fallback: {
+      Buffer: require.resolve('buffer/'),
+    },
+  },
+  resolve: {
+    fallback: {
+      "buffer": require.resolve('buffer/'),
+    }
+};
